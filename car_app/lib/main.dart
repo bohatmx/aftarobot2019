5913059ca54e1ab0dc0f0da969a52cfe8e31d38a
Aubrey Malabie <malengatiger@gmail.com>
2018-12-24 12:07:28 +0200

    test map search attched to app - fundmental building block for app

--- car_app/lib/main.dart
+++ car_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:aftarobotlibrary/util/city_map_search.dart';
+import 'package:aftarobotlibrary/util/functions.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
       title: 'CarApp',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
@@ -50,65 +53,42 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _startCitySearchMap() {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => CityMapSearch()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'AftaRobot CarApp',
+              style: Styles.purpleReallyLarge,
             ),
             Text(
-              '$_counter',
+              'The new Autonomous Vehicle App',
               style: Theme.of(context).textTheme.display1,
+            ),
+            Text(
+              'Transportation Data collection without human intervention or hassle.',
+              style: Styles.tealBoldSmall,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _startCitySearchMap,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.map),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
