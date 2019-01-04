58ca463b9f48cb8f951dde10e64b11b5bffb5746
Aubrey Malabie <malengatiger@gmail.com>
2019-01-04 23:50:09 +0200

    initial commit - added taxi to commuter nearby messaging

---
+++ vehicle/android/app/src/main/java/com/aftarobot/vehicle/CommuterMessageReceiver.java
@@ -0,0 +1,37 @@
+package com.aftarobot.vehicle;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+public class CommuterMessageReceiver extends BroadcastReceiver {
+
+    public static final String TAG = CommuterMessageReceiver.class.getSimpleName();
+    public static final String MESSAGE_RECEIVED_INTENT = "com.aftarobot.MESSAGE_RECEIVED_INTENT";
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "\nonReceive: ##### - \uD83D\uDD35 - \uD83D\uDD35  RECEIVED IN BACKGROUND!!!!");
+        Nearby.getMessagesClient(context).handleIntent(intent, new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                Log.d(TAG, " ✅ Found commuter message, background: " + new String(message.getContent()));
+                LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+                Intent m = new Intent(MESSAGE_RECEIVED_INTENT);
+                m.putExtra("message", new String(message.getContent()));
+                localBroadcastManager.sendBroadcast(m);
+            }
+
+            @Override
+            public void onLost(Message message) {
+                Log.i(TAG, "Lost commuter message; via PendingIntent: " + message);
+            }
+        });
+    }
+}
+
---
+++ vehicle/android/app/src/main/java/com/aftarobot/vehicle/VehicleTypeDTO.java
@@ -0,0 +1,81 @@
+package com.aftarobot.vehicle;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.Gson;
+
+import java.io.Serializable;
+
+/**
+ * Created by aubreymalabie on 9/10/16.
+ */
+
+public class VehicleTypeDTO implements Serializable, Comparable<VehicleTypeDTO>{
+
+    public VehicleTypeDTO() {
+    }
+
+    public VehicleTypeDTO( String make, String model, int capacity) {
+        this.capacity = capacity;
+        this.make = make;
+        this.model = model;
+    }
+
+    private int capacity;
+    private String vehicleTypeID, make, model, countryID;
+
+    public String getVehicleTypeID() {
+        return vehicleTypeID;
+    }
+
+    public void setVehicleTypeID(String vehicleTypeID) {
+        this.vehicleTypeID = vehicleTypeID;
+    }
+
+    public String getCountryID() {
+        return countryID;
+    }
+
+    public void setCountryID(String countryID) {
+        this.countryID = countryID;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public int compareTo(@NonNull VehicleTypeDTO o) {
+        if (this.make == null || this.model == null) {
+            return 0;
+        }
+        if (o.make == null || o.model == null) {
+            return 0;
+        }
+        String m1 = this.make.concat(" ").concat(this.getModel());
+        String m2 = o.make.concat(" ").concat(o.getModel());
+        return m1.compareTo(m2);
+    }
+
+    static final Gson GSON = new Gson();
+}
