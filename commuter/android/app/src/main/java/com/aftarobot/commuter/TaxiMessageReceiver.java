90db7c87e30ec15f6b9d07e123ba6b36315a63d6
Aubrey Malabie <malengatiger@gmail.com>
2019-01-04 23:51:00 +0200

    initial commit - nearby messaging added II

---
+++ commuter/android/app/src/main/java/com/aftarobot/commuter/TaxiMessageReceiver.java
@@ -0,0 +1,35 @@
+package com.aftarobot.commuter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+public class TaxiMessageReceiver extends BroadcastReceiver {
+    public static final String TAG = TaxiMessageReceiver.class.getSimpleName();
+    public static final String MESSAGE_RECEIVED_INTENT = "com.aftarobot.MESSAGE_RECEIVED_INTENT";
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "\nonReceive: ##### - \uD83D\uDD35 - \uD83D\uDD35  RECEIVED IN BACKGROUND!!!!");
+        Nearby.getMessagesClient(context).handleIntent(intent, new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                Log.d(TAG, " ✅ Found message, in background: " + new String(message.getContent()));
+                LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+                Intent m = new Intent(MESSAGE_RECEIVED_INTENT);
+                m.putExtra("message", new String(message.getContent()));
+                localBroadcastManager.sendBroadcast(m);
+            }
+
+            @Override
+            public void onLost(Message message) {
+                Log.i(TAG, "Lost message via PendingIntent: " + message);
+            }
+        });
+    }
+}
---
+++ commuter/android/app/src/main/java/com/aftarobot/commuter/VehicleDTO.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aftarobot.commuter;
+
+import java.io.Serializable;
+
+/**
+ * @author Aubrey Malabie, Esq.
+ */
+public class VehicleDTO implements Serializable, Comparable<VehicleDTO> {
+
+    private String vehicleID, ownerID,
+            associationID, countryID, ownerName, associationName;
+    private VehicleTypeDTO vehicleType;
+    private String year, stringDate,
+            vehicleReg;
+    private Boolean selected;
+
+    public String getAssociationName() {
+        return associationName;
+    }
+
+    public void setAssociationName(String associationName) {
+        this.associationName = associationName;
+    }
+    public Boolean getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Boolean selected) {
+        this.selected = selected;
+    }
+
+    public String getStringDate() {
+        return stringDate;
+    }
+
+    public void setStringDate(String stringDate) {
+        this.stringDate = stringDate;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(String vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public String getAssociationID() {
+        return associationID;
+    }
+
+    public void setAssociationID(String associationID) {
+        this.associationID = associationID;
+    }
+
+    public String getCountryID() {
+        return countryID;
+    }
+
+    public void setCountryID(String countryID) {
+        this.countryID = countryID;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+
+    public VehicleTypeDTO getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleTypeDTO vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+    public String getVehicleReg() {
+        return vehicleReg;
+    }
+
+    public void setVehicleReg(String vehicleReg) {
+        this.vehicleReg = vehicleReg;
+    }
+    @Override
+    public int compareTo(VehicleDTO o) {
+        if (this.vehicleReg == null || o.vehicleReg == null) {
+            return 0;
+        }
+        return this.vehicleReg.compareTo(o.vehicleReg);
+    }
+
+}
